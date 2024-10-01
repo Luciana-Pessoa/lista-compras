37fe7ed2597108cc50140c8e3b68483428cd3cdd
lu9932323258@gmail.com <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-10-01 16:15:05 -0300

    lista

--- js/excluirItem.js
+++ js/excluirItem.js
@@ -1,12 +1,17 @@
 import { verificarListaComprados } from "./verificarListaComprados";
 
+const listaDeCompras = document.getElementById("lista-de-compras");
+const listaComprados = document.getElementById("lista-comprados");
+
 const excluirItem = (elemento) => {
     let confirmacao = confirm("Tem certeza que deseja excluir esse item?")
 
     if(confirmacao) {
         elemento.remove();
 
-        verificarListaComprados();
-        verificarListaVazia();
+        verificarListaComprados(lista-comprados);
+        verificarListaVazia(lista-de-compras);
     }
 }
+
+export {excluirItem };
